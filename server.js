7daaea499c7ea218568182a70676a17940cda98f
MeghnaObalashetty <meghnadayanand96@gmail.com>
2023-09-16 17:24:31 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const budget = {
+const port = 3002;
+const fs = require('fs');
+const budget1 = {
     myBudget: [{
             title: 'Eat out',
             budget: 35
@@ -16,14 +17,31 @@ const budget = {
         },
     ]
 };
+// Reading the JSON file and store its contents in a variable
+let budget = null;
+fs.readFile('budget.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading JSON file:', err);
+        return;
+    }
+    budget = JSON.parse(data);
+    console.log('JSON data loaded:', budget);
+});
+app.get('/budget', (req, res) => {
+    if (budget) {
+        res.json(budget);
+    } else {
+        res.status(500).json({ error: 'Budget data not available' });
+    }
+});
 
 app.use('/', express.static('public'));
 
 app.get('/hello', (req, res) => {
     res.send('Hello world');
 });
-app.get('/budget', (req, res) => {
-    res.json(budget);
+app.get('/budget1', (req, res) => {
+    res.json(budget1);
 });
 app.listen(port, () => {
     console.log(`API served at http://localhost:${port}`);
